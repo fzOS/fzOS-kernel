00176f56306d708a3198fb5a9fd5fee9c09aa738
CorsairCat <1274056321@qq.com>
2020-02-18 08:31:51 +0800

    change bg color

--- helloworld.c
+++ helloworld.c
@@ -3,9 +3,9 @@
 
 void kernel_main(KernelInfo info) {
   graphics_init(info.gop);
-  graphics_clear_screen(0x00000000);
+  graphics_clear_screen(0x00eed0db);
   U32 default_font_color;
-  default_font_color = 0xffffff00;
+  default_font_color = 0x00ffffff;
   kernel_log_print_string("Hello World! I am fzOS.\n", default_font_color);
   kernel_log_print_string("kernel version: fzKernel 0.1.0", default_font_color);
 }
