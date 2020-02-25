6318f66e280be80a561d18eab3b9375cc6b9338c
CorsairCat <1274056321@qq.com>
2020-02-25 21:22:09 +0800

    fix errors

--- helloworld.c
+++ helloworld.c
@@ -7,14 +7,16 @@ void kernel_main(KernelInfo info) {
   U32 default_font_color;
   default_font_color = 0x00ffffff;
   kernel_log_print_string(" Hello World! I am fzOS.", default_font_color);
-  kernel_log_print_string("\n Kernel version: fzKernel-0.1.0", default_font_color);
+  kernel_log_print_string("\n Kernel version: fzKernel-0.1.1", default_font_color);
 
   kernel_log_print_string("\n Kernel lowest address: ", default_font_color);
   kernel_print_U64_hex(info.kernel_lowest_address, default_font_color);
   kernel_log_print_string("\n Kernel Memory Usage: ", default_font_color);
   kernel_print_U64_hex(info.kernel_page_count, default_font_color);
   kernel_log_print_string("\n Memory map pointer: ", default_font_color);
-  kernel_print_U8_hex(*info.memory_map, default_font_color);
+  U64 memorymappointer;
+  memorymappointer = (U64) info.memory_map;
+  kernel_print_U64_hex(memorymappointer, default_font_color);
   kernel_log_print_string("\n Memory map size: ", default_font_color);
   kernel_print_U64_hex(info.mem_map_size, default_font_color);
   kernel_log_print_string("\n Memory Map Descriptor: ", default_font_color);
