ad182918a449f5c4bec056a51a39779c78b9d552
CorsairCat <1274056321@qq.com>
2020-02-28 09:40:41 +0800

    update bootup variables

--- include/kernel.h
+++ include/kernel.h
@@ -7,7 +7,8 @@
 #define _KERNEL_H_
 
 typedef struct {
-  void *xdsp_address;
+  void *rsdp_address;
+  //U8 *uefi_rt_pointer;
   U8 *memory_map;
   U64 mem_map_size;
   U64 mem_map_descriptor_size;
