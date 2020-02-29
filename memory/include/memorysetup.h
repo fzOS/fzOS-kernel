364e5522ce5ba549615ebec5dcd9efb255e2c02b
CorsairCat <1274056321@qq.com>
2020-02-29 14:02:59 +0800

    get efer cr0-4 value

--- memory/include/memorysetup.h
+++ memory/include/memorysetup.h
@@ -1,5 +1,5 @@
 #include <types.h>
-#include <graphic.h>
+#include <graphics.h>
 #include <kerneldisplay.h>
 
 void memory_init(U64 mem_map_descriptor_size,U64 mem_map_size,U8 *memory_map,U32 color);
--- memory/memorysetup.c
+++ memory/memorysetup.c
@@ -16,12 +16,29 @@ void memory_init(U64 mem_map_descriptor_size,U64 mem_map_size,U8 *memory_map,U32
         kernel_print_U64_hex(*memmappointer, color);
         memmappointer = memmappointer + 2;
     }*/
-    U64 CR3Value;
-    __asm__ __violate__(
-        "movl %%cr3,%0"
-        : "g"(CR3Value)
+    U64 CR0Value,CR3Value,CR4Value,PageDictionAddr,RFLAGValue,EFERValue;
+    __asm__(
+        "movq %%cr3,%0;"
+        "movq %%cr4,%1;"
+        "movq %%cr0,%2;"
+        //"movq %%rflags,%3;"
+        "movq $0xC0000080,%%rcx;"
+        "rdmsr;"
+        "movq %%rax, %4;"
+        : "=g"(CR3Value), "=g"(CR4Value), "=g"(CR0Value), "=g"(RFLAGValue), "=g"(EFERValue)
         : 
-        : "memory");
+        : "%rcx","%rax","%rdx","memory");
+    //kernel_log_print_string("\n RFLAGS value:", color);
+    //kernel_print_U64_hex(RFLAGValue, color);
+    kernel_log_print_string("\n EFER value:", color);
+    kernel_print_U64_hex(EFERValue, color);
+    kernel_log_print_string("\n CR0 value:", color);
+    kernel_print_U64_hex(CR0Value, color);
     kernel_log_print_string("\n CR3 value:", color);
     kernel_print_U64_hex(CR3Value, color);
+    PageDictionAddr = (CR3Value&0xffffffffffff000) >> 12;
+    kernel_log_print_string("\n Page Diction Address Base:", color);
+    kernel_print_U64_hex(PageDictionAddr, color);
+    kernel_log_print_string("\n CR4 value:", color);
+    kernel_print_U64_hex(CR4Value, color);
 }
