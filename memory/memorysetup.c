7ccfe0bb64427e5c7c8e1e1c754ade4c9d149f1d
CorsairCat <1274056321@qq.com>
2020-02-29 17:15:38 +0800

    add 4-level page verify

--- memory/memorysetup.c
+++ memory/memorysetup.c
@@ -30,6 +30,8 @@ void memory_init(U64 mem_map_descriptor_size,U64 mem_map_size,U8 *memory_map,U32
         : "%rcx","%rax","%rdx","memory");
     //kernel_log_print_string("\n RFLAGS value:", color);
     //kernel_print_U64_hex(RFLAGValue, color);
+    U8 four_level_paging_flag = 0;
+    U8 ia32e_mode_flag = 0;
     kernel_log_print_string("\n EFER value:", color);
     kernel_print_U64_hex(EFERValue, color);
     kernel_log_print_string("\n CR0 value:", color);
@@ -41,4 +43,21 @@ void memory_init(U64 mem_map_descriptor_size,U64 mem_map_size,U8 *memory_map,U32
     kernel_print_U64_hex(PageDictionAddr, color);
     kernel_log_print_string("\n CR4 value:", color);
     kernel_print_U64_hex(CR4Value, color);
+    if (((EFERValue >> 8) & 0x1) == 0x1){//check ia32-EFER.LME status
+        ia32e_mode_flag = 1;
+        kernel_log_print_string("\n CPU is at IA-32E mode", color);
+        four_level_paging_flag = four_level_paging_flag + 1;
+    }
+    if (((CR4Value >> 5) & 0x1) == 0x1){//check CR4.PAE status
+        kernel_log_print_string("\n CR4.PAE is enabled", color);
+        four_level_paging_flag = four_level_paging_flag + 1;
+    }
+    if (((CR0Value >> 31) & 0x1) == 0x1){//check CR0.PG status
+        kernel_log_print_string("\n CR0.PG is enabled", color);
+        four_level_paging_flag = four_level_paging_flag + 1;
+    }
+    if (four_level_paging_flag == 3){// check if four level paging is enabled
+        kernel_log_print_string("\n 4-Level Paging is enabled; Attemp to handle it now.",color);
+        // Start read UEFI page settings and creating new page tables
+    }
 }
