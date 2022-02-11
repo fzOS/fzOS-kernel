28ff75d8e2364548001c05a6697bce61f97079e8
fhh200000 <1041154549@qq.com>
2022-02-11 20:43:45 +0800

    Update:pidgen #?

--- coldpoint/automata/ctrl_inst.c
+++ coldpoint/automata/ctrl_inst.c
@@ -1,4 +1,5 @@
 #include <coldpoint/automata/ctrl_inst.h>
+#include <common/bswap.h>
 static inline void relative_jump(thread* t)
 {
     U8 offset1=t->code->code[t->pc];
@@ -14,11 +15,19 @@ cpstatus opcode_goto(thread* t)
 }
 cpstatus opcode_tableswitch(thread* t)
 {
+    U64 orig_pc=t->pc;
     //skip the padding.
     if(t->pc&0x3) {
         t->pc = (t->pc&(~0x3)) +1;
     }
-
+    //Default Offset.
+    I32* p = (I32*)&t->code->code[t->pc];
+    I32 default_offset = bswap32(*p);
+    p++;
+    I32 high_val = bswap32(*p);
+    p++;
+    I32 low_val = bswap32(*p);
+    p++;
     return COLD_POINT_SUCCESS;
 }
 cpstatus opcode_lookupswitch(thread* t);
