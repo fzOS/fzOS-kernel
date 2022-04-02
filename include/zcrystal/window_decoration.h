6d90dcf6061e48d550dd1027a8dd6115c49b3743
fhh200000 <1041154549@qq.com>
2022-04-02 17:26:48 +0800

    Update:Window decoration image.

---
+++ include/zcrystal/window_decoration.h
@@ -0,0 +1,10 @@
+#ifndef WINDOW_DECORATION
+#define WINDOW_DECORATION
+#include <types.h>
+extern const U32 g_close_button_active[];
+extern const U32 g_close_button_inactive[];
+extern const U32 g_maximize_button_active[];
+extern const U32 g_maximize_button_inactive[];
+extern const U32 g_minimize_button_active[];
+extern const U32 g_minimize_button_inactive[];
+#endif
